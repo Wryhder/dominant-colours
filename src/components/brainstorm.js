d22ad7aad46d4a29e6fc5942d78efcf784f394d3
Wryhder <jemimahsummers@yahoo.com>
2023-03-09 22:29:12 +0100

    Create/highlight todos

---
+++ src/components/brainstorm.js
@@ -0,0 +1,9 @@
+// TODO:
+// - Choose a theme/background story
+// - Design basic frontend elements
+// - Accept images either via the device’s camera (if it has one) or a file dialog.
+// - Intelligently determine three of the most dominant colors in the image 
+//   and present it to the user. The dominant colors in this case are the colors
+//   that appear most in a given image.
+// - Generate the RGB, HSB, CYMK and HEX color codes for each dominant color
+// - Add the ability to save the generated color palette with all the above information
